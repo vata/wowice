00c7288714679e9f3cc16d13177d78eb32eb0f96
mvt3am <mvt3am@6123f428-43f9-11de-952f-710615d9d0d5>
2009-05-19 00:19:11 +0000

    WoW Ice: - Adding GNU General Public License v3 Code for all files

--- trunk/WoWICE/src/wowice-shared/crc32.cpp
+++ trunk/WoWICE/src/wowice-shared/crc32.cpp
@@ -1,4 +1,18 @@
 /*
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+/*
  * $Id: crc32.c,v 1.1.1.1 1996/02/18 21:38:12 ylo Exp $
  * $Log: crc32.c,v $
  * Revision 1.1.1.1  1996/02/18 21:38:12  ylo
