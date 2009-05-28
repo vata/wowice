3406293c7199c27ea249b4895f5de64b7f4ed35a
mvt3am <mvt3am@6123f428-43f9-11de-952f-710615d9d0d5>
2009-05-28 21:33:44 +0000

    Trunk WoWICE: - Added: collision_tools

---
+++ WoWICE/src/collision_tools/assembler/vmap/DebugCmdLogger.cpp
@@ -0,0 +1,112 @@
+/*
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "DebugCmdLogger.h"
+
+namespace VMAP
+{
+
+    bool CommandFileRW::appendCmd(const Command& pCommand)
+    {
+        #ifdef _DEBUG
+        bool result = false;
+        if(iWritingEnabled || pCommand.isCoreCmd())
+        {
+            FILE* f = fopen(iFileName.c_str(), "ab");
+            if(f)
+            {
+                result = true;
+                if(fwrite(&pCommand, sizeof(Command), 1, f) != 1) { result = false; }
+                fclose(f);
+            }
+        }
+        else
+        {
+            result = true;
+        }
+        return result;
+        #else
+        return true;
+        #endif
+    }
+
+    //=========================================================
+
+    bool CommandFileRW::appendCmds(const Array<Command>& pCmdArray)
+    {
+        #ifdef _DEBUG
+        bool result = false;
+        if(iWritingEnabled)
+        {
+            FILE* f;
+            if(resetfile)
+                f = fopen(iFileName.c_str(), "wb");
+            else
+                f = fopen(iFileName.c_str(), "ab");
+            resetfile = false;
+
+            if(f)
+            {
+                result = true;
+                for(int i=0; i<pCmdArray.size(); ++i)
+                {
+                    if(fwrite(&pCmdArray[i], sizeof(Command), 1, f) != 1) { result = false; break; }
+                }
+                fclose(f);
+            }
+        }
+        else
+        {
+            result = true;
+        }
+        return result;
+        #else
+        return true;
+        #endif
+    }
+
+    //=========================================================
+
+    bool CommandFileRW::getNewCommands(Array<Command>& pCmdArray)
+    {
+        bool result = false;
+        FILE* f = fopen(iFileName.c_str(), "rb");
+        if(f)
+        {
+            Command cmd;
+            if(fseek(f, iLastPos, SEEK_SET) == 0) { result = true; }
+            while(result)
+            {
+                if(fread(&cmd, sizeof(Command), 1, f) != 1)
+                {
+                    result = false;
+                }
+                iLastPos = ftell(f);
+                if(cmd.getType() == STOP)
+                {
+                    break;
+                }
+                pCmdArray.append(cmd);
+            }
+            fclose(f);
+        }
+        if(result)
+        {
+            iCommandArray.append(pCmdArray);
+        }
+        return(result);
+    }
+    //========================================================
+}
