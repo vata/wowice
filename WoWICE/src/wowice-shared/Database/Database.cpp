3c79a24f9e985a7c2b1a470c897dd2dea452241b
mvt3am <mvt3am@6123f428-43f9-11de-952f-710615d9d0d5>
2009-06-13 07:39:59 +0000

    Trunk WoWICE: - Added missing sql files - Clean up & fix

--- WoWICE/src/wowice-shared/Database/Database.cpp
+++ WoWICE/src/wowice-shared/Database/Database.cpp
@@ -13,6 +13,7 @@
  * this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+
 #include "DatabaseEnv.h"
 #include "../CrashHandler.h"
 #include "../NGLog.h"
--- WoWICE/src/wowice-shared/svn_revision.h
+++ WoWICE/src/wowice-shared/svn_revision.h
@@ -5,7 +5,7 @@
 #define SVN_REVISION_H
 
 static const char * BUILD_TAG = "TRUNK";
-static int BUILD_REVISION = 15;
+static int BUILD_REVISION = 2620;
 
 #endif		 // SVN_REVISION_H
 
