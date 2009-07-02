2ed6de90bf91c243231e1b22c5771c74e8ac6bb1
mvt3am <mvt3am@6123f428-43f9-11de-952f-710615d9d0d5>
2009-07-02 10:56:02 +0000

    Trunk WoWICE - Added: Licence 3 for all files

--- trunk/WoWICE/src/scripts/src/MiscScripts/GameObjectTeleportTable.cpp
+++ trunk/WoWICE/src/scripts/src/MiscScripts/GameObjectTeleportTable.cpp
@@ -1,4 +1,19 @@
 /*
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
  *	This script will add support for an SQL table called gameobject_teleports.
  *	This table can be used to teleport players when they use a game object such
  *  as a door or portal. Any object used in this table should exist in the
